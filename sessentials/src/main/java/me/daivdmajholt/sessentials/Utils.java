11fba070e1c8ff8a4f285301ab20aaebef380b72
David Majholt <d.boy.dragoz@gmail.com>
2026-01-21 20:47:07 +0100

    Added new util for require None Null Added version.txt file

--- sessentials/src/main/java/me/daivdmajholt/sessentials/Utils.java
+++ sessentials/src/main/java/me/daivdmajholt/sessentials/Utils.java
@@ -7,4 +7,8 @@ public class Utils {
 	public static String cc(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
 	}
+
+	public static <T> T reqNN(T obj) {
+		return java.util.Objects.requireNonNull(obj);
+	}
 }
