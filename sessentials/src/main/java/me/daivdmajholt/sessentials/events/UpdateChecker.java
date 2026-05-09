560c9b1a503981ae087f1f3ddb6a227b2b12a37e
KatyTye <d.boy.dragoz@gmail.com>
2026-05-09 12:58:49 +0200

    feat: Added updatechecker chat messages

--- sessentials/src/main/java/me/daivdmajholt/sessentials/events/UpdateChecker.java
+++ sessentials/src/main/java/me/daivdmajholt/sessentials/events/UpdateChecker.java
@@ -1,10 +1,13 @@
 package me.daivdmajholt.sessentials.events;
 
-import me.daivdmajholt.sessentials.Main;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-import java.net.URL;
+import static me.daivdmajholt.sessentials.Utils.cc;
+
+import java.net.URI;
 import java.util.Scanner;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+import me.daivdmajholt.sessentials.Main;
 
 public class UpdateChecker {
 
@@ -15,20 +18,41 @@ public class UpdateChecker {
         this.apiUrl = "https://raw.githubusercontent.com/KatyTye/SEssentials/main/version.txt";
     }
 
-     public void check() {
-         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-             try (Scanner scanner = new Scanner(new URL(apiUrl).openStream())) {
-                 String latest = scanner.nextLine();
-                 String current = plugin.getDescription().getVersion();
+    public void check() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try (Scanner scanner = new Scanner(URI.create(apiUrl).toURL().openStream())) {
+                String latest = scanner.nextLine();
+                String current = plugin.getDescription().getVersion();
 
-                 if (!current.equals(latest)) {
-                     plugin.getLogger().info("New version available: " + latest);
-                 } else {
-                     plugin.getLogger().info("You are running the latest version.");
-                 }
-             } catch (Exception e) {
-                 plugin.getLogger().warning("Could not check for updates.");
-             }
-         });
-     }
+                if (!current.equals(latest)) {
+                    plugin.getLogger().info("New version available: " + latest);
+                } else {
+                    plugin.getLogger().info("You are running the latest version.");
+                }
+            } catch (Exception e) {
+                plugin.getLogger().warning("Could not check for updates.");
+            }
+        });
+    }
+
+    public void messageCheck(CommandSender sender, Runnable callback) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try (Scanner scanner = new Scanner(URI.create(apiUrl).toURL().openStream())) {
+                String latest = scanner.nextLine();
+                String current = plugin.getDescription().getVersion();
+
+                sender.sendMessage(cc(" &fCurrent Version: &a" + current));
+
+                if (!current.equals(latest)) {
+                    sender.sendMessage(cc(" &cYour plugin version is outdated."));
+                } else {
+                    sender.sendMessage(cc(" &aYour plugin is up to date."));
+                }
+            } catch (Exception e) {
+                plugin.getLogger().warning("Could not check for updates.");
+            }
+
+            callback.run();
+        });
+    }
 }
