d00de4d7ce5f06b636a2de121111edab5dab026a
KatyTye <d.boy.dragoz@gmail.com>
2026-05-09 13:06:04 +0200

    feat: Added force gamemode on join event

--- sessentials/src/main/java/me/daivdmajholt/sessentials/events/JoinMessage.java
+++ sessentials/src/main/java/me/daivdmajholt/sessentials/events/JoinMessage.java
@@ -1,11 +1,12 @@
 package me.daivdmajholt.sessentials.events;
 
 import me.daivdmajholt.sessentials.Main;
+
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import static me.daivdmajholt.sessentials.Utils.cc;
 
@@ -17,6 +18,24 @@ public class JoinMessage implements Listener {
     public void onPlayerJoinEvent(PlayerJoinEvent event){
         Player player = event.getPlayer();
         event.setJoinMessage(cc(plugin.getConfig().getString("messages.join") + player.getName()));
+
+        if (plugin.getConfig().getBoolean("settings.force-gamemode")) {
+            try {
+                GameMode gm;
+                switch (plugin.getConfig().getString("settings.default-gamemode").toLowerCase()) {
+                    case "s", "survival" -> gm = GameMode.SURVIVAL;
+                    case "c", "creative" -> gm = GameMode.CREATIVE;
+                    case "a", "adventure" -> gm = GameMode.ADVENTURE;
+                    case "sp", "spectator" -> gm = GameMode.SPECTATOR;
+                    default -> {
+                        gm = GameMode.SURVIVAL;
+                    }
+                }
+                player.setGameMode(gm);
+            } catch (NumberFormatException e) {
+                plugin.getLogger().warning("Could not force gamemode on " + player.getName() + ", is it a valid gamemode?");
+            }
+        }
     }
 
 }
