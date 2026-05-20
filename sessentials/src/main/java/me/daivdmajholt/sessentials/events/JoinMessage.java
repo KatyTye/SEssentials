81e2e13debf4d4148467ce96bbc699b0624850a3
KatyTye <d.boy.dragoz@gmail.com>
2026-05-20 09:57:35 +0200

    feat: Added plugin advertisements when toggled

--- sessentials/src/main/java/me/daivdmajholt/sessentials/events/JoinMessage.java
+++ sessentials/src/main/java/me/daivdmajholt/sessentials/events/JoinMessage.java
@@ -22,12 +22,23 @@ public class JoinMessage implements Listener {
     private final Main plugin = Main.plugin;
 
     @EventHandler
-    public void onPlayerJoinEvent(PlayerJoinEvent event){
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         event.setJoinMessage(cc(plugin.getConfig().getString("messages.join") + player.getName()));
 
         Main.databaseManager.registerPlayer(player, 0, 0);
-        
+
+        if (plugin.getConfig().getBoolean("do-plugin-advertisements")) {
+            plugin.getLogger().info(
+                    " This server is using the SEssentials plugin from: https://www.curseforge.com/minecraft/bukkit-plugins/sessentials");
+            player.sendMessage("");
+            player.sendMessage(cc(" &f&lWELCOME TO THE &6&lSERVER!"));
+            player.sendMessage(cc(" &fThis server is using &6SEssentials&f for basically everything!"));
+            player.sendMessage(cc(
+                    " &fTry this plugin on your server at: &6https://www.curseforge.com/minecraft/bukkit-plugins/sessentials"));
+            player.sendMessage("");
+        }
+
         if (plugin.getConfig().getBoolean("settings.force-gamemode")) {
             try {
                 GameMode gm;
@@ -42,29 +53,30 @@ public class JoinMessage implements Listener {
                 }
                 player.setGameMode(gm);
             } catch (NumberFormatException e) {
-                plugin.getLogger().warning("Could not force gamemode on " + player.getName() + ", is it a valid gamemode?");
+                plugin.getLogger()
+                        .warning("Could not force gamemode on " + player.getName() + ", is it a valid gamemode?");
             }
         }
 
         if (plugin.getConfig().getBoolean("settings.spawn-on-join")) {
             File dataFolder = plugin.getDataFolder();
-			File file = new File(dataFolder, "spawn.yml");
+            File file = new File(dataFolder, "spawn.yml");
 
-			FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+            FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 
-			String worldName = cfg.getString("world", "world");
-			World world = Bukkit.getWorld(worldName);
-			if (world == null) {
-				plugin.getLogger().warning("World not found: " + worldName);
-			}
+            String worldName = cfg.getString("world", "world");
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                plugin.getLogger().warning("World not found: " + worldName);
+            }
 
-			double x = cfg.getDouble("x", 0.0);
-			double y = cfg.getDouble("y", 0.0);
-			double z = cfg.getDouble("z", 0.0);
-			float yaw = (float) cfg.getDouble("yaw", 0.0);
-			float pitch = (float) cfg.getDouble("pitch", 0.0);
+            double x = cfg.getDouble("x", 0.0);
+            double y = cfg.getDouble("y", 0.0);
+            double z = cfg.getDouble("z", 0.0);
+            float yaw = (float) cfg.getDouble("yaw", 0.0);
+            float pitch = (float) cfg.getDouble("pitch", 0.0);
 
-			Location loc = new Location(world, x, y, z, yaw, pitch);
+            Location loc = new Location(world, x, y, z, yaw, pitch);
 
             player.teleport(loc);
         }
