1a6cd2121a51629b8e6ce37c68d40c9e7e3bc2f1
KatyTye <d.boy.dragoz@gmail.com>
2026-05-30 11:28:27 +0200

    feat: Added functions to handle warp checks and return of warp names

--- sessentials/src/main/java/me/daivdmajholt/database/DatabaseManager.java
+++ sessentials/src/main/java/me/daivdmajholt/database/DatabaseManager.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -131,6 +133,48 @@ public class DatabaseManager {
 				}
 				return true;
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public List<String> getAllWarps() {
+		List<String> warps = new ArrayList<>();
+
+		String sql = """
+				SELECT name FROM warps
+				""";
+		
+		try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(sql);
+		ResultSet resultSet = statement.executeQuery();) {
+			while (resultSet.next()) {
+				warps.add(resultSet.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return warps;
+	}
+
+	public boolean checkWarpExists(String name) {
+
+		String sql = """
+				SELECT name FROM warps WHERE name = ?
+				""";
+
+			try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(sql)) {
+
+			statement.setString(1, name);
+
+			ResultSet result = statement.executeQuery();
+
+			if (result.next()) {
+				return true;
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
